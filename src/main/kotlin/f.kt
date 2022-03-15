c4afc85971ca1434a31b379b922375622f25c64d
xzdunno <dudnikov.anton1306@yandex.ru>
2022-03-15 10:42:16 +0300

    Done

--- src/main/kotlin/f.kt
+++ src/main/kotlin/f.kt
@@ -1,115 +1,92 @@
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import java.beans.XMLDecoder
-import java.beans.XMLEncoder
-import java.io.*
-import java.util.zip.ZipEntry
-import java.util.zip.ZipInputStream
-import java.util.zip.ZipOutputStream
-import javax.swing.JButton
+import org.redundent.kotlin.xml.xml
+import java.io.File
 import javax.swing.filechooser.FileSystemView
-
 
 @Serializable
 data class JS(val surname:String, val name:String, val secondName:String, val age:Int)
 fun main(){
-    val kok=FileSystemView.getFileSystemView()
-    val lol=File.listRoots()
-    for(disk in lol){
-        println("Название:$disk")
-        println("Тип: ${kok.getSystemTypeDescription(disk)}")
-        println("Объём диска:${disk.totalSpace}")
-        println("Свободное пространство:${disk.freeSpace}")
-        println("Метка:$disk")
-    }
-    val file=File("C:\\Для лабы","laba1_1.txt")
-    file.createNewFile()
-    println("Введите строку")
-    val str=readLine().toString()
-    file.writeText(str.toString())
-    println(file.readText())
-    file.delete()
-
-    println("Введите свою фамилию")
-    val sur= readLine().toString()
-    println("Введите своё имя")
-    val name= readLine().toString()
-    println("Введите своё отчество")
-    val second= readLine().toString()
-    println("Введите свой возраст")
-    val age=readLine().toString().toInt()
-    val json = Json.encodeToString<JS> (JS(sur,name,second,age))
-    val fileJs=File("C:\\Для лабы","laba1_2.txt")
-    fileJs.writeText(json)
-    println(fileJs.readText())
-    fileJs.delete()
-
-    val file3=File("C:\\untitled5","laba1_4.xml")
-    println("Введите строку")
-    val xml= readLine().toString()
-    val e = XMLEncoder(
-        BufferedOutputStream(
-            FileOutputStream("laba1_4.xml")
-        )
-    )
-    e.writeObject(JButton(xml))
-    e.close()
-    val d = XMLDecoder(
-        BufferedInputStream(
-            FileInputStream("laba1_4.xml")
-        )
-    )
-    println(d.readObject().toString())
-    d.close()
-file3.delete()
-    val file4=File("C:\\untitled5","notes.txt")
-    file4.writeText("Что угодно")
-    println()
-    val filename = "C:\\untitled5\\notes.txt"
-    try {
-        ZipOutputStream(FileOutputStream("C:\\untitled5\\output.zip")).use { zout ->
-            FileInputStream(filename).use { fis ->
-                val entry1 = ZipEntry("notes.txt")
-                zout.putNextEntry(entry1)
-                // считываем содержимое файла в массив byte
-                val buffer = ByteArray(fis.available())
-                fis.read(buffer)
-                // добавляем содержимое к архиву
-                zout.write(buffer)
-                // закрываем текущую запись для новой записи
-                zout.closeEntry()
-            }
-        }
-    } catch (ex: Exception) {
-        println(ex.message)
-    }
-    try {
-        ZipInputStream(FileInputStream("C:\\untitled5\\output.zip")).use { zin ->
-            var entry: ZipEntry
-            var name: String
-            var size: Long
-            while (zin.nextEntry.also { entry = it } != null) {
-                name = entry.name // получим название файла
-                size = entry.size // получим его размер в байтах
-                System.out.printf("File name: %s \t File size: %d \n", name, size)
-
-                // распаковка
-                val fout = FileOutputStream("C:\\untitled5\\new$name")
-                var c = zin.read()
-                while (c != -1) {
-                    fout.write(c)
-                    c = zin.read()
+    var choice=true
+    while (choice) {
+        println("1.Вывод информации о дисках")
+        println("2.Работа с файлом")
+        println("3.Работа с JSON")
+        println("4.Работа с XML")
+        println("5.Прервать выполнение программы")
+            when(readLine().toString()){
+                "1"->{
+                    val kok = FileSystemView.getFileSystemView()
+                    val lol = File.listRoots()
+                for (disk in lol) {
+                    println("Название:$disk")
+                    println("Тип: ${kok.getSystemTypeDescription(disk)}")
+                    println("Объём диска:${disk.totalSpace}")
+                    println("Свободное пространство:${disk.freeSpace}")
+                    println("Метка:$disk")
                 }
-                fout.flush()
-                zin.closeEntry()
-                fout.close()
             }
-        }
-    } catch (ex: java.lang.Exception) {
-        println(ex.message)
+                "2"->{
+                    val file = File("C:\\Для лабы", "laba1_1.txt")
+                    file.createNewFile()
+                    println("Введите строку")
+                    val str = readLine().toString()
+                    file.writeText(str)
+                    println(file.readText())
+                    println("Хотите удалить файл? 1.Да 2.Нет")
+                    if(readLine().toString()=="1")
+                    file.delete()
+                }
+                "3"->{
+                    println("Введите свою фамилию")
+                    val sur = readLine().toString()
+                    println("Введите своё имя")
+                    val name = readLine().toString()
+                    println("Введите своё отчество")
+                    val second = readLine().toString()
+                    println("Введите свой возраст")
+                    val age = readLine().toString().toInt()
+                    val json = Json.encodeToString<JS>(JS(sur, name, second, age))
+                    val fileJs = File("C:\\Для лабы", "laba1_2.txt")
+                    fileJs.writeText(json)
+                    println(fileJs.readText())
+                    println("Хотите удалить файл? 1.Да 2.Нет")
+                    if(readLine().toString()=="1")
+                    fileJs.delete()
+                }
+
+                "4"->{
+                    val people = xml("people") {
+                        xmlns = "http://example.com/people"
+                        "person" {
+                            attribute("id", 1)
+                            "firstName" {
+                                -"John"
+                            }
+                            "lastName" {
+                                -"Doe"
+                            }
+                            "phone" {
+                                -"555-555-5555"
+                            }
+                        }
+                    }
+
+                    val asString = people.toString()
+                    val filexml = File("C:\\Для лабы", "labXml.txt")
+                    filexml.writeText(asString)
+                    println(filexml.readText())
+                    println("Хотите удалить файл? 1.Да 2.Нет")
+                    if(readLine().toString()=="1")
+                    filexml.delete()
+                }
+                "5"->{
+                    print("До новых встреч!")
+                    choice=false
+                }
+
+                else->println("Подумай над своим поведением и выбери нормально!")
+            }
     }
-    file4.delete()
-    val file5=File("C:\\untitled5","output.zip")
-    file5.delete()
 }
